b21a34c8a81fa1019649da7588529daf2b5cd764
nadimmostafa <nadimmostafa334@gmail.com>
2024-10-06 15:26:39 +0600

    add banner section

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import img1 from "../../assets/Group 34991.png";
+import './style.css'
+
+const Banner = () => {
+  return (
+    <div className="background p-6">
+      <div className=" lg:flex justify-around">
+        <div className="lg:mt-40 lg;w-[70%]">
+          <h2 className="text-4xl lg:text-6xl font-bold text-white">
+            Your Best Medical Help Center
+          </h2>
+          <p className="text-sm py-2 text-white">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis a
+            ducimus dolorum voluptate minima veniam laudantium.
+          </p>
+          <button className="btn btn-info my-2">Visite</button>
+        </div>
+        <div>
+        <img src={img1} alt="" />
+      </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default Banner;
