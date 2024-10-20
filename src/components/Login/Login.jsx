902d9984e9695f8f2651bc1134d424bad0c4ab80
Md Nadim Mostofa <126006304+Nadimmo@users.noreply.github.com>
2024-10-21 01:39:12 +0600

    Add files via upload

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,49 @@
-import React from "react";
-import img from "../../assets/Frame.png";
+import React, { useContext } from "react";
 import "../Register/style.css";
 import { Link } from "react-router-dom";
+import img from '../../assets/Frame.png'
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
 const Login = () => {
+  const {login} = useContext(AuthContext)
+
+  const handlerLogin = (event)=>{
+    event.preventDefault()
+    const from = event.target;
+    const email = from.email.value
+    const password = from.password.value
+
+    login(email,password) 
+    .then(res =>{
+      console.log(res.user)
+      if(res.user){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "User Login Successfully",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
+    .catch(err =>{
+      // console.log(err.message)
+      alert(err.message)
+
+    })
+    // const result = {email,password}
+    // console.log(result)
+  }
+  
   return (
     <div className="my-5">
-      <div className="hero back rounded-2xl  min-h-screen p-6">
+      <div className="hero background rounded-2xl  min-h-screen p-6">
         <div className="hero-content rounded-xl bg-white flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <img src={img} alt="" className="back rounded-xl" />
+            <img src={img} alt="" className="background rounded-xl" />
           </div>
           <div className="card  w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form onSubmit={handlerLogin} className="card-body">
               <h1 className="text-center text-2xl lg:text-4xl font-bold">
                 Login now!
               </h1>
@@ -25,6 +57,7 @@ const Login = () => {
                   placeholder="email"
                   className="input input-bordered"
                   required
+                  name="email"
                 />
               </div>
               <div className="form-control">
@@ -36,6 +69,7 @@ const Login = () => {
                   placeholder="password"
                   className="input input-bordered"
                   required
+                  name="password"
                 />
                 {/* Todo: recaptcha */}
               </div>
